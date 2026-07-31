dad86aadf004508d5e4de90b9f94d6119974b3a2
anishfathima-techcmantix <anishfathima@techcmantix.com>
2026-07-31 16:41:24 +0530

    project setup completed

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: { path: "prisma/migrations" },
+  schema: "server/prisma/schema.prisma",
+  migrations: { path: "server/prisma/migrations" },
   datasource: { url: process.env["DATABASE_URL"] },
 });
